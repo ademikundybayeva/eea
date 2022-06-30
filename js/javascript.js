a4184477eb9e35e2f98d3d4e5d244b3e64f1d551
ademikundybayeva <98976113+ademikundybayeva@users.noreply.github.com>
2022-06-30 11:40:30 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,17 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
 const osm =
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
+            maxZoom: 8,
             attribution: 'OpenStreetMap contributors',
       })
 osm.addTo(map)
 
 function popUPinfo(feature, layer) {
-      layer.bindPopup(feature.properties.NIMI)
+      layer.bindPopup(feature.properties.Aadress)
 }
 
-async function addDistrictsGeoJson(url) {
-      const response = await fetch(url)
-      const data = await response.json()
-      const polygons = L.geoJson(data, {
-            onEachFeature: popUPinfo,
-      })
-      polygons.addTo(map)
-}
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
 
-async function addCelltowersGeoJson(url) {
+async function addGasStationsGeoJson(url) {
       const response = await fetch(url)
       const data = await response.json()
       const markers = L.geoJson(data)
@@ -28,7 +19,7 @@ async function addCelltowersGeoJson(url) {
       clusters.addLayer(markers)
       clusters.addTo(map)
 }
-addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+addCelltowersGeoJson('geojson/gas_stations.geojson')
 
 function defaultMapSettings() {
       map.setView([58.373523, 26.716045], 12)
